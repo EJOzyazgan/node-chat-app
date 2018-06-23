9c8b7534a516c1de593db2a464368d67588c6a7f
EJ Ozyazgan <ejozyazgan@gmail.com>
2018-06-23 15:29:03 -0700

    Emit new message

--- server/server.js
+++ server/server.js
@@ -1,25 +1,19 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const sockerIO = require('socket.io');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port  = process.env.PORT || 3000;
 
 let app = express();
 let server = http.createServer(app);
-let io = sockerIO(server);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log("New Client Connected");
-
-    socket.emit('newMessage', {
-        from: "ej",
-        text: "hi how are you",
-        createdAt: 123
-    });
 
     socket.on('createEmail', (newEmail) => {
         console.log("createdEmail", newEmail);
@@ -27,6 +21,11 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log("createdMessage", message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
